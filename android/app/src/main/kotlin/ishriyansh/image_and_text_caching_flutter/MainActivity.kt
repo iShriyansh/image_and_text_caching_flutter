233addc645b5c8ec04a9ef9add4e80eff79dbf25
ishriyansh <ishreeyansh@gmail.com>
2019-12-20 17:17:56 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/ishriyansh/image_and_text_caching_flutter/MainActivity.kt
@@ -0,0 +1,13 @@
+package ishriyansh.image_and_text_caching_flutter
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
